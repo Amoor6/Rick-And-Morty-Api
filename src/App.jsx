b139c8f5d627b258998f484fe34325a08caecafa
Amoor navasartian <amoor6navasartian@gmail.com>
2024-01-31 22:43:01 +0330

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useEffect, useState } from "react";
+import { Characters, allCharacters } from "../data/data";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import CharecterDetail from "./components/CharecterDetail";
+import CharecterList from "./components/CharecterList";
+import Navbar, { SearchResult } from "./components/Navbar";
 
-function App(){
-  return <div className="app"> <Navbar/> </div>
+function App() {
+  const [characters, setCharacters] = useState(allCharacters);
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+
+  return (
+    <div className="app">
+      <Navbar>
+        <SearchResult numOfResult={characters.length} />
+      </Navbar>
+      <Main>
+        <CharecterList characters={characters} />
+        <CharecterDetail />
+      </Main>
+    </div>
+  );
+}
+
+export default App;
+
+function Main({ children }) {
+  return <div className="main">{children}</div>;
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,35 @@
-function Navbar() {
+import { HeartIcon } from "@heroicons/react/24/outline";
+
+function Navbar({ children }) {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <Logo />
+      <Search />
+      {children}
+      <Favourites />
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+function Logo() {
+  return <div className="navbar__logo">Logo</div>;
+}
+
+function Search() {
+  return <input type="text" className="text-field" placeholder="search..." />;
+}
+
+export function SearchResult() {
+  return <div className="navbar__result">{numOfResult} characters</div>;
+}
+
+function Favourites() {
+  return (
+    <button className="heart">
+      <HeartIcon className="icon" />
+      <span className="badge">4</span>
+    </button>
+  );
+}
